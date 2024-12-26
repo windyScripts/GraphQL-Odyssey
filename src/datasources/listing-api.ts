2cc4042ef5e419e55f8dd57c50438b71ec0ee177
Aravind U Shenoy <aravindushenoy@Aravinds-MacBook-Air.local>
2024-12-26 14:28:33 +0530

    13 Resolver chains

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,6 +1,6 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 
-import { Listing } from "../types";
+import { Listing, Amenity } from "../types";
 
 export class ListingAPI extends RESTDataSource{
     baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
@@ -10,6 +10,10 @@ export class ListingAPI extends RESTDataSource{
     };
     getListing(listingId: string): Promise<Listing> {
         return this.get<Listing>(`listings/${listingId}`)
+    }
+    getAmenities(listingId: string): Promise<Amenity[]>{
+        console.log("Making a follow-up call for amenities with ", listingId);
+        return this.get<Amenity[]>(`listings/${listingId}/amenities`)
     }
 }
 
